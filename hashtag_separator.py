4335033b579fba0665a6f0a58a67775b734d94a5
Dino Mujkic <dinomujkic2017@u.northwestern.edu>
2017-12-10 17:58:10 +0100

    Preprocessing done

---
+++ hashtag_separator.py
@@ -0,0 +1,36 @@
+#https://stackoverflow.com/questions/8870261/how-to-split-text-without-spaces-into-list-of-words/11642687#11642687
+
+from math import log
+
+# Build a cost dictionary, assuming Zipf's law and cost = -math.log(probability).
+words = open('./words-by-frequency.txt').read().split()
+wordcost = dict((k, log((i+1)*log(len(words)))) for i,k in enumerate(words))
+maxword = max(len(x) for x in words)
+
+def infer_spaces(s):
+    """Uses dynamic programming to infer the location of spaces in a string
+    without spaces."""
+
+    # Find the best match for the i first characters, assuming cost has
+    # been built for the i-1 first characters.
+    # Returns a pair (match_cost, match_length).
+    def best_match(i):
+        candidates = enumerate(reversed(cost[max(0, i-maxword):i]))
+        return min((c + wordcost.get(s[i-k-1:i], 9e999), k+1) for k,c in candidates)
+
+    # Build the cost array.
+    cost = [0]
+    for i in range(1,len(s)+1):
+        c,k = best_match(i)
+        cost.append(c)
+
+    # Backtrack to recover the minimal-cost string.
+    out = []
+    i = len(s)
+    while i>0:
+        c,k = best_match(i)
+        assert c == cost[i]
+        out.append(s[i-k:i])
+        i -= k
+
+    return " ".join(reversed(out))
