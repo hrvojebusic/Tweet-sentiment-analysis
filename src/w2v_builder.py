c92402a35853fe7cb22495817d9fbe88c525eebe
hrvojebusic <hrvoje@protonmail.com>
2017-12-20 19:55:12 +0100

    [ADD] W2V builder utility functions - documentation.

--- src/w2v_builder.py
+++ src/w2v_builder.py
@@ -3,6 +3,20 @@ import re
 
 
 def create_w2v(tweets, text_column, size, w2v):
+    """
+    Creates word embeddings vector representation for the
+    tweets passed as data frame, in specified column. Vector
+    size is specified by the second argumnent and needs to
+    correspond to the vector size of the given pretrained
+    vocabulary - the third argument.
+    INPUT:
+        tweets: data frame containing tweets
+        text_column: name of the column where tweet content is stored
+        size: vector size
+        w2v: pretrained vocabulary vector mapping
+    OUTPUT:
+        word embeddings vectors for the given tweets
+    """
     content = tweets[text_column].values
     content = list(map(lambda x: re.split(r'\s+', x), content))
     content = list(map(lambda x: build_word_vector(x, size, w2v), content))
